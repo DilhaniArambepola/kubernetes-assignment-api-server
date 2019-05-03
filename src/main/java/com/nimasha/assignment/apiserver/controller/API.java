4703c2d5f08432de7c9bfa1d28b969e127417708
unknown <nimashandk94@gmail.com>
2019-05-03 22:00:00 +0530

    initial commit

---
+++ src/main/java/com/nimasha/assignment/apiserver/controller/API.java
@@ -0,0 +1,46 @@
+package com.nimasha.assignment.apiserver.controller;
+
+import com.nimasha.assignment.apiserver.model.StateData;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/")
+public class API {
+
+
+    @GetMapping(path = "codeToState")
+    public ResponseEntity<Object> codeToState(@RequestParam String code) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("code", code);
+
+        ResponseEntity<StateData> responseEntity = new RestTemplate().getForEntity(
+                "http://34.66.114.249:8000/codeToState/{code}", StateData.class,
+                uriVariables);
+
+        StateData response = responseEntity.getBody();
+        if (isEmpty(response)) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().body(response.getName());
+    }
+
+    @GetMapping(path = "stateToCode")
+    public ResponseEntity<Object> stateToCode(@RequestParam String state) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("state", state);
+
+        ResponseEntity<StateData> responseEntity = new RestTemplate().getForEntity("http://34.66.114.249:8000/stateToCode/{state}", StateData.class, uriVariables);
+
+        StateData response = responseEntity.getBody();
+        if (isEmpty(response)) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().body(response.getAbbreviation());
+    }
+
+    private boolean isEmpty(StateData stateData) {
+        return stateData == null;
+    }
+}
---
+++ src/main/java/com/nimasha/assignment/apiserver/model/StateData.java
@@ -0,0 +1,23 @@
+package com.nimasha.assignment.apiserver.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StateData {
+    private Long id;
+    private String name;
+    private String abbreviation;
+	public Object getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Object getAbbreviation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
